8583e83193819339dd767af20b85091f36e2ecfc
PdskDev <prestadesk@gmail.com>
2023-07-09 20:37:13 +0200

    Add two main models

---
+++ src/models/Company.ts
@@ -0,0 +1,14 @@
+interface Company {
+  id: number;
+  name: string;
+  address: string;
+  address2: string;
+  town: string;
+  city: string;
+  postCode: string;
+  country: string;
+  contactEmail: string;
+  numberOfStaff: string;
+  industry: string;
+  isActive: boolean;
+}
---
+++ src/models/Contact.ts
@@ -0,0 +1,11 @@
+interface Contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  jobtitle: string;
+  status: string;
+  isActive: boolean;
+  relatedCompany: string;
+  notes: string;
+}
